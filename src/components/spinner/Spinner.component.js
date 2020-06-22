09ba6e399f8d3c5f850db301e24ba9b361fac75d
David Ezequiel Assen <dassen@solstice.com>
2020-06-22 04:31:46 -0300

    added style

--- src/components/spinner/Spinner.component.js
+++ src/components/spinner/Spinner.component.js
@@ -2,8 +2,17 @@ import React from "react";
 import Spinner from "react-bootstrap/Spinner";
 
 const Loader = ({ loading, children }) => {
-  if (loading) {            
-    return <Spinner animation="border" />;
+  if (loading) {
+    return (
+      <Spinner
+        animation="border"
+        style={{
+          position: "absolute",
+          left: "13vw",
+          top: "15vh",
+        }}
+      />
+    );
   }
 
   return <div>{children}</div>;
